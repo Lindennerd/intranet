c324ee02882aca79980970a54f9dbada97ecbeb3
BR TRD WK Luiz Paulo Lindenmaier <br-trdwklplsilva@kpmg.com.br>
2020-10-06 17:26:11 -0300

    sidebar

--- routes/security.js
+++ routes/security.js
@@ -6,7 +6,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.render('login', {title: "Login"});
+  res.render('login', {title: "Login", layout: 'login-layout'});
 });
 
 router.post('/', function(req, res, next) {
