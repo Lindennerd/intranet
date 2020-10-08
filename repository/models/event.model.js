dbee9fdb27e56b5b28be03dfbee3bec2b8ecbfb8
Luiz Paulo <lindennerd@gmail.com>
2020-10-07 21:51:37 -0300

    começando a schedule

---
+++ repository/models/event.model.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const eventSchema = new mongoose.Schema({
+    name: String,
+    description: String,
+    attendees: [{
+        id: mongoose.SchemaTypes.ObjectId,
+        name: String
+    }],
+    date: Date,
+    duration: String,
+    createdAt: {type: Date, default: new Date()},
+    canceled: {type: Boolean, default: false}
+}, {collection: 'event'});
+
+module.exports =  mongoose.model('event', eventSchema);
--- repository/models/schedule.model.js
+++
@@ -1,5 +0,0 @@
-const mongoose = require('mongoose');
-
-const scheduleSchema = new mongoose.Schema({
-    
-});
